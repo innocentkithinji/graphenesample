66b5dfedbcde7e95cf9aeff24dcb0f5d194f96c0
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-08-17 19:55:25 +0300

    Complete on my Side

--- app/Units/models.py
+++ app/Units/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Unit(models.Model):
     name = models.CharField(max_length=60)
-    alternative_name = models.CharField(max_length=60, null=True)
+    alternative_name = models.CharField(max_length=60, null=True, blank=True)
 
     def __str__(self):
         return self.name
