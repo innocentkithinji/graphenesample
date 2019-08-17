2d2ad372ec0443613415d8b5324a9671ff39c34a
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-08-17 23:16:21 +0300

    Complete on my Side

--- app/Packages/admin.py
+++ app/Packages/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import BuyerPackage, FarmPackage
+from .models import BuyerPackage, FarmPackage, Period
 
 admin.site.register(BuyerPackage)
 admin.site.register(FarmPackage)
+admin.site.register(Period)
