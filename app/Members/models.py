c5b0fc0e367e3105357385c374a6e69edccb6ddb
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-08-23 19:51:44 +0300

    Minor changes

--- app/Members/models.py
+++ app/Members/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Member(models.Model):
     phoneNumber = models.CharField(max_length=12, unique=True)
     uid = models.CharField(max_length=60, unique=True)
-    fcm_id = models.CharField(max_length=70)
+    fcm_id = models.CharField(max_length=512)
 
     def __str__(self):
         return self.phoneNumber
