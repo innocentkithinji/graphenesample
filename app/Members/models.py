d0fb6263f994b94e3fffde7e474e887097543c80
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-08-23 20:02:32 +0300

    minor changes

--- app/Members/models.py
+++ app/Members/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Member(models.Model):
-    phoneNumber = models.CharField(max_length=12, unique=True)
+    phoneNumber = models.CharField(max_length=20, unique=True)
     uid = models.CharField(max_length=60, unique=True)
     fcm_id = models.CharField(max_length=1024)
 
