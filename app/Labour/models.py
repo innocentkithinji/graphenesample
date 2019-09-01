612cc97e388992867684e22877674cab85c4b9ea
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-09-01 10:54:00 +0300

    minor changes

--- app/Labour/models.py
+++ app/Labour/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-class LabourServices(models.Model):
+class LabourService(models.Model):
     title = models.CharField(max_length=70)
     description = models.TextField()
 
