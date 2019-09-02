8d770344609aa38652093682a6435d8df48b2746
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-09-03 01:24:39 +0300

    Added Wards

--- app/Wards/models.py
+++ app/Wards/models.py
@@ -7,3 +7,6 @@ from County.models import County
 class Ward(models.Model):
     name = models.CharField(max_length=50)
     county = models.ForeignKey(County, on_delete=models.CASCADE, related_name="wards")
+
+    def __str__(self):
+        return self.name
