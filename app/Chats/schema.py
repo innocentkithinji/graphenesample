57f3819a1cf15153d202a12d7d4f3f2800ea5af4
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-09-12 12:34:17 +0300

    minor changes

--- app/Chats/schema.py
+++ app/Chats/schema.py
@@ -3,6 +3,7 @@ from Farm.models import Farm
 from Buyer.models import Buyer
 import graphene
 from graphene_django import DjangoObjectType
+from pyfcm import FCMNotification
 
 
 class ChatType(DjangoObjectType):
@@ -36,5 +37,22 @@ class AddChat(graphene.Mutation):
         return AddChat(chat=chat)
 
 
+class SendNotification(graphene.Mutation):
+    class Arguments:
+        partyId = graphene.Int(required=True)
+        type = graphene.String(required=True)
+        Message = graphene.String(required=True)
+        title = graphene.String(required=True)
+        action = graphene.String(required=True)
+
+    def mutate(self, info, partyId, type, Message, title, action):
+        buyer = Buyer.objects.get(id=partyId)
+        push = FCMNotification(
+            api_key="AAAAr7bm4Pw:APA91bGzMCMzPkoSPvqXkbSFGe5cRBjMDWRKV8tIkVGg76UwcYARrmMWrQjkx9fDsG"
+                    "GcrrfcDbkLuhvmmeDtzPsdW22MnNzND_14rEMVTLOpGXL67G8tj88sKQrKrs0iIhWxXwqGEbiA")
+        print(buyer.owner.fcm_id)
+
+
 class Mutation(graphene.ObjectType):
     addChat = AddChat.Field()
+    notify = SendNotification.Field()
