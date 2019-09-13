4f4d310627e67b63d41852471614fcb7043b90c4
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-09-13 19:41:56 +0300

    minor changes

--- app/Chats/schema.py
+++ app/Chats/schema.py
@@ -48,15 +48,24 @@ class SendNotification(graphene.Mutation):
         action = graphene.String(required=True)
 
     def mutate(self, info, partyId, type, Message, title, action):
-        buyer = Buyer.objects.get(id=partyId)
-        push = FCMNotification(
-            api_key="AAAAr7bm4Pw:APA91bGzMCMzPkoSPvqXkbSFGe5cRBjMDWRKV8tIkVGg76UwcYARrmMWrQjkx9fDsG"
-                    "GcrrfcDbkLuhvmmeDtzPsdW22MnNzND_14rEMVTLOpGXL67G8tj88sKQrKrs0iIhWxXwqGEbiA")
-        regid = buyer.owner.fcm_id
-        print(regid)
-        # regid = "e7vP0PmAWio:APA91bFZq1kVw7sNo6xee1FJPuxQ69GQm6R-H5G8jmt5c1Pf1a6fyFDqgJqsKSk99gc1OFGVnKnIRt6r6KLYi8-JPXyLULzctALTsS-aItsrmZ3MsX6BCqExEFVKigFME7vyz9hankNm"
-        result = push.notify_single_device(registration_id=regid, message_title=title, message_body=Message)
-        print(result)
+        if type is "buyer":
+            buyer = Buyer.objects.get(id=partyId)
+            push = FCMNotification(
+                api_key="AAAAr7bm4Pw:APA91bGzMCMzPkoSPvqXkbSFGe5cRBjMDWRKV8tIkVGg76UwcYARrmMWrQjkx9fDsG"
+                        "GcrrfcDbkLuhvmmeDtzPsdW22MnNzND_14rEMVTLOpGXL67G8tj88sKQrKrs0iIhWxXwqGEbiA")
+            regid = buyer.owner.fcm_id
+            print(regid)
+            result = push.notify_single_device(registration_id=regid, message_title=title, message_body=Message)
+            print(result)
+        else:
+            farm = Farm.objects.get(id=partyId)
+            push = FCMNotification(
+                api_key="AAAAr7bm4Pw:APA91bGzMCMzPkoSPvqXkbSFGe5cRBjMDWRKV8tIkVGg76UwcYARrmMWrQjkx9fDsG"
+                        "GcrrfcDbkLuhvmmeDtzPsdW22MnNzND_14rEMVTLOpGXL67G8tj88sKQrKrs0iIhWxXwqGEbiA")
+            regid = farm.owner.fcm_id
+            print(regid)
+            result = push.notify_single_device(registration_id=regid, message_title=title, message_body=Message)
+            print(result)
 
 
 class Mutation(graphene.ObjectType):
