bbefdb14abf3743f89dba48c7fc3f9d1fc79f74d
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-09-18 13:04:07 +0300

    minor changes

--- app/Agent/models.py
+++ app/Agent/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 # Create your models here.
 class Agent(models.Model):
-    name = models.CharField(max_length=10)
-    email = models.CharField(max_length=10)
+    name = models.CharField(max_length=256)
+    email = models.CharField(max_length=256)
     code = models.IntegerField(unique=True, default=random.randint(1, 999999))
 
     def __str__(self):
