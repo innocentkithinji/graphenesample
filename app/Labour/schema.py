b16e4ab419a26cdf66a28140bc6c75aaba6a9005
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-09-20 19:08:38 +0300

    Added Labour Changes

--- app/Labour/schema.py
+++ app/Labour/schema.py
@@ -1,16 +1,21 @@
 from django.db.models import Q
-from .models import LabourService
+from .models import LabourService, PayPeriod, LaboursRequest
 import graphene
 from graphene_django import DjangoObjectType
-
+from app.Wards.models import Ward
 
 class LabourType(DjangoObjectType):
     class Meta:
         model = LabourService
 
+class LabourRequestType(DjangoObjectType):
+    class Meta:
+        model = LaboursRequest
+
 
 class Query(graphene.ObjectType):
     services = graphene.List(LabourType, name=graphene.String())
+    labourRequests = graphene.List(LabourRequestType, ward=graphene.Int(), service=graphene.Int())
 
     def resolve_services(self, info, name=None):
         srv = LabourService.objects.all()
@@ -22,3 +27,8 @@ class Query(graphene.ObjectType):
             srv = srv.filter(filters)
 
         return srv
+
+    def resolve_labourRequests(self, info, ward=None, service=None):
+        lrs = LaboursRequest.objects.all()
+
+        return lrs
