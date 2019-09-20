bf49fb6bdd20811eec05b29c112d9a544277d9cf
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-09-20 18:13:20 +0300

    Added Labour Changes

--- app/Labour/models.py
+++ app/Labour/models.py
@@ -14,13 +14,13 @@ class LabourService(models.Model):
         return self.title
 
 
-# class LaboursRequest(models.Model):
-#     PERIOD_CHOICES = [(1, "Day"), (2, "Week"), (3, "Month")]
-#     ward = models.ForeignKey(Ward, related_name="labourServiceRequest", on_delete=models.CASCADE)
-#     farm = models.ForeignKey(Farm, on_delete=models.CASCADE, null=True, related_name="LabourRequests", blank=True)
-#     buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE, null=True, related_name="LabourRequests", blank=True)
-#     service = models.ForeignKey(LabourService, on_delete=models.CASCADE, null=False)
-#     startDate = models.DateTimeField(null=False)
-#     periodNumber = models.IntegerField(null=False)
-#     pay = models.FloatField(null=False)
-#     payPeriod = models.CharField(max_length=30, choices=PERIOD_CHOICES, default=1)
+class LaboursRequest(models.Model):
+    PERIOD_CHOICES = [(1, "Day"), (2, "Week"), (3, "Month")]
+    ward = models.ForeignKey(Ward, related_name="labourServiceRequest", on_delete=models.CASCADE)
+    farm = models.ForeignKey(Farm, on_delete=models.CASCADE, null=True, related_name="LabourRequests", blank=True)
+    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE, null=True, related_name="LabourRequests", blank=True)
+    service = models.ForeignKey(LabourService, on_delete=models.CASCADE, null=False)
+    startDate = models.DateTimeField(null=False)
+    periodNumber = models.IntegerField(null=False)
+    pay = models.FloatField(null=False)
+    payPeriod = models.CharField(max_length=30, choices=PERIOD_CHOICES, default=1)
