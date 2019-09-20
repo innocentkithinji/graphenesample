b9113686a40a54eed1c61d3ec49134d2fb2d7826
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-09-20 17:44:10 +0300

    Added Labourer Services Request

--- app/Labour/models.py
+++ app/Labour/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from Wards.models import Ward
+from Farm.models import Farm
+from Buyer.models import Buyer
 
 
 # Create your models here.
@@ -9,3 +12,15 @@ class LabourService(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class LaboursRequest(models.Model):
+    PERIOD_CHOICES = [(1, "Day"), (2, "Week"), (3, "Month")]
+    ward = models.ForeignKey(Ward, related_name="labourServiceRequest", on_delete=models.CASCADE)
+    farm = models.ForeignKey(Farm, on_delete=models.CASCADE, null=True, related_name="LabourRequests", blank=True)
+    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE, null=True, related_name="LabourRequests", blank=True)
+    service = models.ForeignKey(LabourService, on_delete=models.CASCADE, null=False)
+    startDate = models.DateTimeField(null=False)
+    periodNumber = models.IntegerField(null=False)
+    pay = models.FloatField(null=False)
+    payPeriod = models.CharField(max_length=30, choices=PERIOD_CHOICES, default=1)
