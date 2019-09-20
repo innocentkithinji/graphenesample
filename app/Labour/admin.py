10b87663b854f8ef2e92a26c2fcda2e0efa5c0eb
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-09-20 19:25:50 +0300

    Added Labour Changes

--- app/Labour/admin.py
+++ app/Labour/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import LabourService, PayPeriod
+from .models import LabourService, PayPeriod, LaboursRequest
 # Register your models here.
 
 admin.site.register(LabourService)
-# admin.site.register(LaboursRequest)
+admin.site.register(LaboursRequest)
 admin.site.register(PayPeriod)
