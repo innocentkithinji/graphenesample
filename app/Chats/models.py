794c09d247a2188a6e9ca11efb8c2f5b55ecd7dd
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-09-26 18:31:25 +0300

    Changes to chat

--- app/Chats/models.py
+++ app/Chats/models.py
@@ -7,7 +7,7 @@ from Sales.models import Sale
 class Chat(models.Model):
     partyA = models.ForeignKey(Buyer, related_name="buyerChats", on_delete=models.CASCADE)
     partyB = models.ForeignKey(Farm, related_name="farmChats", on_delete=models.CASCADE)
-    sale = models.ForeignKey(Sale, related_name="chats", on_delete=models.CASCADE(), null=True)
+    sale = models.ForeignKey(Sale, related_name="chats", on_delete=models.CASCADE, null=True)
     docId = models.TextField()
 
     def __str__(self):
