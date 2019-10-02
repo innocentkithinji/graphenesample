dc990925e71b183c3a5e590d8bc48cd0103add11
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-10-02 09:06:55 +0300

    Prices Ready for dump

--- app/Prices/models.py
+++ app/Prices/models.py
@@ -7,5 +7,5 @@ from Products.models import Product
 class Prices(models.Model):
     price = models.IntegerField(null=False)
     county = models.ForeignKey(County, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE())
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     time = models.DateTimeField(auto_now_add=True)
