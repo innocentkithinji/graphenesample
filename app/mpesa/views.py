ea02da6fb842e248fdc6a03dc659d8ff45b32617
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-10-05 16:21:47 +0300

    updated confirmURL

--- app/mpesa/views.py
+++ app/mpesa/views.py
@@ -28,5 +28,8 @@ def mpesa_validation(request):
 def mpesa_confirmation(request):
     response = request.body
     recieved = json.loads(response)
-    print(recieved)
+    # {'TransactionType': '', 'TransID': 'NJ551HAKRP', 'TransTime': '20191005161329', 'TransAmount': '300.00',
+    #  'BusinessShortCode': '600755', 'BillRefNumber': 'BY6543211', 'InvoiceNumber': '', 'OrgAccountBalance': '',
+    #  'ThirdPartyTransID': '', 'MSISDN': '254708374149', 'FirstName': 'John', 'MiddleName': 'J.', 'LastName': 'Doe'}
+    print(recieved["BillRefNumber"])
     return HttpResponse(status=200)
