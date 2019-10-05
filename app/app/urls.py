955be3fa4005361b924d7f64dafc61fa37245307
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-10-05 17:24:39 +0300

    Payment recieved DB created

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.views.static import serve
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
-from  mpesa import views
+from  Mpesa import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
