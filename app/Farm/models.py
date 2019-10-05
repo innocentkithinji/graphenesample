f328b051cda72eb86d1470277a32dadaa198d53a
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-10-05 14:30:08 +0300

    Graphql auto add accounts iv

--- app/Farm/models.py
+++ app/Farm/models.py
@@ -22,9 +22,9 @@ class Farm(models.Model):
     Ward = models.ForeignKey(Ward, on_delete=models.CASCADE, related_name="farms", null=True)
     package = models.ForeignKey(FarmPackage, on_delete=models.SET_NULL, null=True)
     package_update_date = models.DateTimeField(auto_now_add=True)
-    valid = models.BooleanField(True)
     owner = models.ForeignKey(Member, on_delete=models.CASCADE, related_name="farms", blank=True)
     accounts = models.CharField(max_length=20, blank=True, null=True, unique=True)
+    active = models.BooleanField(blank=False, null=False, default=False)
 
     def __str__(self):
         return self.name
