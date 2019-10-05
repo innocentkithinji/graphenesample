01ad03aea8d705c503bb2176ad2ca5aba8bb1d1a
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-10-05 13:38:38 +0300

    Farmer account added

--- app/Farm/models.py
+++ app/Farm/models.py
@@ -5,7 +5,17 @@ from Members.models import Member
 from Wards.models import Ward
 import random
 
+
 # Create your models here.
+
+def uniqueacoount():
+    account = f"FM{random.randint(1, 9)}{random.randint(111, 999)}{random.randint(111, 888)}"
+    Query = Farm.objects.get(accounts=account)
+    if Query.len() == 0:
+        uniqueacoount()
+    return account
+
+
 class Farm(models.Model):
     name = models.CharField(max_length=50)
     county = models.ForeignKey(County, on_delete=models.CASCADE, related_name="farms")
@@ -14,7 +24,8 @@ class Farm(models.Model):
     package_update_date = models.DateTimeField(auto_now_add=True)
     valid = models.BooleanField(True)
     owner = models.ForeignKey(Member, on_delete=models.CASCADE, related_name="farms", blank=True)
-    accounts = models.CharField(max_length=20, blank=False, null=False, unique=True, default=f"FM{random.randint(1,9)}{random.randint(111,999)}{random.randint(111,888)}")
+    accounts = models.CharField(max_length=20, blank=False, null=False, unique=True,
+                                default=uniqueacoount())
 
     def __str__(self):
         return self.name
