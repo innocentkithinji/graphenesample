4dd039aa5fc08ee788f8032d2a7eff178b878a08
Innocent Kithinji <innocentkithinji9@gmail.com>
2019-10-29 20:54:25 +0300

    Setting Changed

--- app/app/settings.py
+++ app/app/settings.py
@@ -56,7 +56,8 @@ INSTALLED_APPS = [
     'Agent',
     'Labourers',
     'Mpesa',
-    'Prices'
+    'Prices',
+    'BuyerRequests',
 ]
 
 GRAPHENE = {
@@ -97,21 +98,21 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'OPTIONS': {
-#             'read_default_file': '/etc/mysql/my.cnf',
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
+    }
+}
 
 
 # Password validation
